373b3c2ae43019b67dda8ac78425e18db8f3688c
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-07-16 19:37:37 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * <div id="parent"> 
+ *      <div id="child"> 
+ *          <h1> </h1>
+ *      <div>
+ * <div>
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [ React.createElement("h1", {id:"heading1"}, "I am an h1 tag"), React.createElement("h1", {id:"heading2"}, "I am an h1 tag")]));
+// const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
