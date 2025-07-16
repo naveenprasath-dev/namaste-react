0498b337aae228465e3a3fe8c9d37cb297123042
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-07-16 20:50:07 +0530

    episode 2 ignite the app

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
-/**
- * 
- * <div id="parent"> 
- *      <div id="child"> 
- *          <h1> </h1>
- *      <div>
- * <div>
- * 
- * 
- * 
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [ React.createElement("h1", {id:"heading1"}, "I am an h1 tag"), React.createElement("h1", {id:"heading2"}, "I am an h1 tag")]));
 // const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
---
+++ README.md
@@ -0,0 +1,21 @@
+# Namaste react 
+
+
+# Parcel 
+  -Dev build
+  -Local Server
+  -HMR - Hot Module Replacement
+  -File Watching algorithm written in C++
+  -Gives faster build because of caching. Parcel cache file is there.
+  -Image Optimization.
+  -Minification.
+  -Bundler
+  -Compress.
+  -Consistent Hashing 
+  -Code splitting
+  -Differential Bundling
+  -DIagnostics
+  -Error Handling
+  -Https Hosting
+  -Tree Shaking - remove unused code for us.
+  -Different dev and production build.
